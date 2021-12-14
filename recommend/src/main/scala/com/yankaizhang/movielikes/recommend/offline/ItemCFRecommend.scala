23ec832129675b89131d918f6e39dae9191a1da8
dzzhyk <dzzhyk@qq.com>
2021-12-15 01:17:36 +0800

    2021-12-15 01:17:30

--- recommend/src/main/scala/com/yankaizhang/movielikes/recommend/offline/ItemCFRecommend.scala
+++ recommend/src/main/scala/com/yankaizhang/movielikes/recommend/offline/ItemCFRecommend.scala
@@ -2,22 +2,25 @@ package com.yankaizhang.movielikes.recommend.offline
 
 import com.yankaizhang.movielikes.recommend.entity.MongoRatingEntity
 import org.apache.spark.rdd.RDD
-import org.apache.spark.sql.{DataFrame, SparkSession}
+import org.apache.spark.sql.{DataFrame, Dataset, SparkSession}
+import org.apache.spark.storage.StorageLevel
 
 import scala.collection.mutable.ArrayBuffer
+
 
 /**
  * 基于物品的离线协同过滤
  */
 object ItemCFRecommend {
 
-  val MONGO_URL = "mongodb://10.237.53.6:27017/movie-recommend"
+  val MONGO_URL = "mongodb://127.0.0.1:27017/movie-recommend"
   val MONGO_COLLECTION = "ratings"
 
   def main(args: Array[String]): Unit = {
 
     // 初始化spark环境
     val sparkSession = SparkSession.builder()
+      .master("local[*]")
       .appName("ItemCFRecommend")
       .config("spark.network.timeout", "10000000")
 //      .config("spark.mongodb.output.uri", "mongodb://10.237.53.6:27017/spark-output")
@@ -66,12 +69,11 @@ object ItemCFRecommend {
     S0 = sparkSession.sparkContext.union(tmpBuffer)
     tmpBuffer.clear()
 
-    // 求笛卡尔积后求和
-    val S1 = S0.groupByKey().mapPartitions(
-      part => {
-        part.map(tup => (tup._1, tup._2.sum))
-      }
-    ).cache()
+    // 求笛卡尔积后groupBy movie元组 求和
+    val S1 = S0.reduceByKey((value, key) => value + key).persist(StorageLevel.MEMORY_AND_DISK_SER)
+
+    println("S1.count: ", S1.count())
+    println(S1.first().toString())
 
     val S2 = S1.filter(r => {
       val key = r._1
@@ -94,5 +96,6 @@ object ItemCFRecommend {
     _S2.destroy()
     S1.unpersist(true)
 
+    println("相似度矩阵记录数: ", simMatrixRDD.count())
   }
 }
