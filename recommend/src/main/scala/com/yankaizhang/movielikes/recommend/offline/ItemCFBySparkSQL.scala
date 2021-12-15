1162ad8c1e75b737c0dffd2b4812a0b7039e569f
dzzhyk <dzzhyk@qq.com>
2021-12-15 13:43:57 +0800

    SparkSQL实现ItemCF

---
+++ recommend/src/main/scala/com/yankaizhang/movielikes/recommend/offline/ItemCFBySparkSQL.scala
@@ -0,0 +1,114 @@
+package com.yankaizhang.movielikes.recommend.offline
+
+import com.yankaizhang.movielikes.recommend.entity.MongoRatingEntity
+import org.apache.spark.sql.{DataFrame, Dataset, Row, SparkSession}
+import org.apache.spark.storage.StorageLevel
+
+/**
+ * 基于物品的离线协同过滤
+ * SparkSQL实现
+ */
+object ItemCFBySparkSQL {
+
+  val MONGO_URL = "mongodb://127.0.0.1:27017/movie-recommend"
+  val MONGO_COLLECTION = "ratings"
+  val defaultParallelism = 24
+
+  def main(args: Array[String]): Unit = {
+
+    val sparkSession = SparkSession.builder()
+      .master("local[8]")
+      .appName("ItemCFBySparkSQL")
+      .config("spark.network.timeout", "10000000")
+      .config("spark.executor.memory", "12g")
+      .config("spark.executor.cores", "6")
+      .config("spark.default.parallelism", defaultParallelism.toString)
+      .config("spark.sql.shuffle.partitions", defaultParallelism.toString)
+      .config("spark.mongodb.output.uri", "mongodb://127.0.0.1:27017/spark-output.itemCFSQL-simMatrix")
+      .getOrCreate()
+
+    import sparkSession.implicits._
+    val ratingDF: DataFrame = sparkSession.read
+      .format("mongo")
+      .option("uri", MONGO_URL)
+      .option("collection", MONGO_COLLECTION)
+      .load()
+      .as[MongoRatingEntity]
+      .map(entity => (entity.userId, entity.movieId, entity.rating))
+      .toDF("userId", "movieId", "rating")
+
+
+    // 统计每个电影的评分个数，并通过内连接添加到 ratingDF 中
+    val numRatersPerMovie: DataFrame = ratingDF.groupBy("movieId").count().alias("count")
+      .coalesce(defaultParallelism)
+    val ratingWithCountDF: DataFrame = ratingDF.join(numRatersPerMovie, "movieId")
+      .coalesce(defaultParallelism)
+
+
+    // 对每个userId做 笛卡尔积内联
+    ratingWithCountDF.join(ratingWithCountDF, "userId")
+      .toDF("userId", "movieId1", "rating1", "count1", "movieId2", "rating2", "count2")
+      .selectExpr("userId"
+        , "movieId1", "rating1", "count1"
+        , "movieId2", "rating2", "count2"
+        , "rating1 * rating2 as product"
+        , "pow(rating1, 2) as rating1Pow"
+        , "pow(rating2, 2) as rating2Pow")
+      .coalesce(defaultParallelism)
+      .createOrReplaceTempView("joined")
+
+    // 计算求解不同相似度必须的一些数据
+    val sparseMatrix = sparkSession.sql(
+      """
+        |SELECT movieId1
+        |, movieId2
+        |, count(userId) as size
+        |, sum(product) as dotProduct
+        |, sum(rating1) as ratingSum1
+        |, sum(rating2) as ratingSum2
+        |, sum(rating1Pow) as  ratingSumOfSq1
+        |, sum(rating2Pow)  as  ratingSumOfSq2
+        |, first(count1) as count1
+        |, first(count2) as count2
+        |FROM joined
+        |GROUP BY movieId1, movieId2
+      """.stripMargin)
+      .coalesce(defaultParallelism)
+      .persist(StorageLevel.MEMORY_AND_DISK_SER)
+
+    //  计算物品相似度
+    import com.yankaizhang.movielikes.recommend.util.SimilarityMeasures._
+    val sim = sparseMatrix.map(row => {
+      val size = row.getAs[Long](2)
+      val dotProduct = row.getAs[Double](3)
+      val ratingSum1 = row.getAs[Double](4)
+      val ratingSum2 = row.getAs[Double](5)
+      val ratingSumOfSq1 = row.getAs[Double](6)
+      val ratingSumOfSq2 = row.getAs[Double](7)
+      val numRaters1 = row.getAs[Long](8)
+      val numRaters2 = row.getAs[Long](9)
+
+      val coOc = coOccurrence(size, numRaters1, numRaters2)
+      val corr = correlation(size, dotProduct, ratingSum1, ratingSum2, ratingSumOfSq1, ratingSumOfSq2)
+      val cosSim = cosineSimilarity(dotProduct, scala.math.sqrt(ratingSumOfSq1), scala.math.sqrt(ratingSumOfSq2))
+      val impCosSim = improvedCosineSimilarity(dotProduct, math.sqrt(ratingSumOfSq1), math.sqrt(ratingSum2), size, numRaters1, numRaters2)
+      val jaccard = jaccardSimilarity(size, numRaters1, numRaters2)
+
+
+      (row.getInt(0), row.getInt(1), coOc, corr, cosSim, impCosSim, jaccard)
+    }).toDF("movieId_01", "movieId_02", "coOc", "corr", "cosSim", "impCosSim", "jaccard")
+
+
+    //  最终的物品相似度矩阵 A->B B->A
+    val tmp = sim.withColumnRenamed("movieId_01", "tmp")
+      .withColumnRenamed("movieId_02", "movieId_01")
+      .withColumnRenamed("tmp", "movieId_02")
+
+    val simMatrixDS: Dataset[Row] = sim.union(tmp)
+      .repartition(defaultParallelism) //  重新分区，以便数据均匀分布，方便下游用户使用
+      .cache()
+
+    import com.mongodb.spark._
+    MongoSpark.save(simMatrixDS)
+  }
+}
